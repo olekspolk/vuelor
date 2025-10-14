c3ae8b9870efe991f523ab937ad717199f845479
olekspolk <polishchuk256@gmail.com>
2025-10-14 15:26:07 -0600

    feat: add ColorPickerPro component and update router

--- src/core/components/ColorPickerInputHEX.vue
+++ src/core/components/ColorPickerInputHEX.vue
@@ -8,7 +8,7 @@ const rootContext = injectColorPickerContext()
   <div
     class="flex gap-[1px] rounded-[5px] hover:outline-1 outline-[#e6e6e6] focus-within:outline-1 focus-within:outline-[#0d99ff]">
     <input type="text" v-model="rootContext.hexa.value"
-      class="w-full bg-[#f5f5f5] rounded-l-[5px] px-2 h-6 flex-1 focus:outline-none text-[11px]"
+      class="w-full flex-1 bg-[#f5f5f5] rounded-l-[5px] px-2 h-6 focus:outline-none text-[11px]"
       aria-label="Hex color" />
     <input type="text" value="100%" class="w-12 bg-[#f5f5f5] rounded-r-[5px] px-2 h-6 focus:outline-none text-[11px]"
       aria-label="Hex color" />
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 import ColorPicker from '@/demos/ColorPicker.vue'
 import ColorPickerMini from '@/demos/ColorPickerMini.vue'
+import ColorPickerPro from '@/demos/ColorPickerPro.vue'
 
 const routes = [
   { path: '/', component: ColorPicker },
   { path: '/mini', component: ColorPickerMini },
+  { path: '/pro', component: ColorPickerPro },
 ]
 
 export const router = createRouter({
