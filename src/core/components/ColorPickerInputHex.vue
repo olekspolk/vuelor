fc135b6321e8cf7662b3eb30fc50664652d125e1
olekspolk <polishchuk256@gmail.com>
2025-10-27 11:33:35 -0600

    feat: add ColorPickerInputHex component with hex and opacity input fields

---
+++ src/core/components/ColorPickerInputHex.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { UiInputSlots } from '../utils/styles'
+import { injectColorPickerContext } from './ColorPickerRoot.vue'
+
+const props = defineProps<{
+  class?: string,
+  ui?: Partial<UiInputSlots>
+}>()
+
+const rootContext = injectColorPickerContext()
+
+const ui = rootContext.uiSlots('input')
+</script>
+
+<template>
+  <div :class="ui.group(props.ui?.group, props.class)">
+    <input
+      v-model="rootContext.hexa.value"
+      type="text"
+      name="hex"
+      aria-label="Hex"
+      :disabled="rootContext.disabled.value"
+      :class="ui.field(props.ui?.field)"
+    />
+    <input
+      v-if="rootContext.isAlphaEnabled.value"
+      type="text"
+      value="100%"
+      name="opacity"
+      aria-label="Opacity"
+      :disabled="rootContext.disabled.value"
+      :class="ui.field('flex-0 w-12', props.ui?.field)"
+    />
+  </div>
+</template>
