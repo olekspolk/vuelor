b5a3498aa94f9177ceb8749dcb3f1ab0d06107f1
olekspolk <polishchuk256@gmail.com>
2025-11-07 20:26:36 -0700

    fix: color updating loop

--- packages/picker/src/components/ColorPickerRoot.vue
+++ packages/picker/src/components/ColorPickerRoot.vue
@@ -63,7 +63,7 @@ watch(
 
 watch(
   () => props.modelValue,
-  (value: ModelValue, oldValue: ModelValue | undefined) => {
+  (value: ModelValue) => {
     if (
       (value === null) ||
       (props.format === 'object' && typeof value !== 'object') ||
@@ -71,7 +71,7 @@ watch(
     ) {
       // Reset to default value if input value is null or of wrong type
       color.hexa.value = props.defaultValue
-    } else if (!isColorsEqual(value, oldValue)) {
+    } else if (!isColorsEqual(value, color.toFormat(props.format))) {
       // Update internal color state if modelValue has changed from outside
       color.fromFormat(value, props.format)
     }
