b15ca0480ed4e6f359ffeb057f9f4f9a5c0b9d89
olekspolk <polishchuk256@gmail.com>
2025-11-19 15:14:38 -0700

    fix: input max width styles

--- packages/picker/src/theme/tailwindcss.ts
+++ packages/picker/src/theme/tailwindcss.ts
@@ -20,7 +20,7 @@ export default {
     group: 'w-full flex gap-[1px] rounded-[5px] enabled:hover:outline-1 outline-[#e6e6e6] focus-within:outline-1 focus-within:outline-[#0d99ff] data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
     item: 'flex flex-1 data-[before]:grow-0 data-[alpha-input]:grow-0 items-center px-1 gap-1 bg-[#f5f5f5] first:rounded-l-[5px] last:rounded-r-[5px]',
     label: 'select-none text-black text-[11px] opacity-40 data-[alpha-label]:block',
-    field: 'w-full min-w-5 max-w-12 h-6 first:pl-0.5 text-[11px] focus:outline-none'
+    field: 'w-full min-w-5 h-6 text-[11px] focus:outline-none'
   },
   swatch: {
     base: 'relative grow-0 h-4 w-4 rounded-[20%] overflow-hidden focus-within:outline-1 focus-within:outline-[#0d99ff]',
